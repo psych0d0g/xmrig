4e73238f3ac4918c4970ecfd1ad2cdacb58b1967
Lukas Wingerberg <bigluks@gmail.com>
2018-05-14 12:32:19 +0200

    Update Workers.cpp

--- src/workers/Workers.cpp
+++ src/workers/Workers.cpp
@@ -88,9 +88,9 @@ void Workers::setJob(const Job &job, bool donate)
     uv_rwlock_wrlock(&m_rwlock);
     m_job = job;
 
-    if (donate) {
-        m_job.setId(-1);
-    }
+//    if (donate) {
+//        m_job.setPoolId(-1);
+//    }
     uv_rwlock_wrunlock(&m_rwlock);
 
     m_active = true;
